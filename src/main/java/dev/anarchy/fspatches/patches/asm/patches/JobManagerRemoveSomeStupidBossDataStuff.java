a23d8f989b07589c6e10f63e298ce6f3b83fea88
Morgane <git@morgane.dev>
2026-06-08 00:08:46 +0200

    some fixes for the public playtest

---
+++ src/main/java/dev/anarchy/fspatches/patches/asm/patches/JobManagerRemoveSomeStupidBossDataStuff.java
@@ -0,0 +1,41 @@
+package dev.anarchy.fspatches.patches.asm.patches;
+
+import dev.anarchy.fspatches.patches.asm.BasePatch;
+import fr.paladium.common.CommonEventHandler;
+import fr.paladium.palajobs.server.listener.EventsHandler;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import static org.objectweb.asm.Opcodes.RETURN;
+
+// fuck
+public class JobManagerRemoveSomeStupidBossDataStuff extends BasePatch {
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        if (basicClass == null) return null;
+        if (!transformedName.equalsIgnoreCase("fr.paladium.common.CommonEventHandler")) return basicClass;
+
+        ClassNode node = new ClassNode();
+        new ClassReader(basicClass).accept(node, 0);
+
+        for (MethodNode method : node.methods) {
+            if (method.name.equals("onJoin")) {
+                method.instructions.clear();
+                method.tryCatchBlocks.clear();
+
+                InsnList insn = new InsnList();
+                insn.add(new InsnNode(RETURN));
+
+                method.instructions.add(insn);
+            }
+        }
+
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        node.accept(writer);
+        return writer.toByteArray();
+    }
+}
